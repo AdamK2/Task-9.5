ba958764c2d76ac1d3b8bfb13629eabeb25e2b97
AdamK2 <ad200@gazeta.pl>
2018-04-23 21:13:21 +0100

    Change html & js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
-var a = 2;
-var b = 3;
-var a = prompt("Type value a");
-var b = prompt("Type value b");
-var value = (a * a) - (2 * a * b) - (b * b);
-console.log("Result is: "+ value); 
-
-if (value > 0) {
-    console.log("Result positive")
-} else if (value < 0) {
-    console.log("Result negative")
-} else {
-    console.log("Result equals zero")
+var hello = document.getElementsByClassName('button'); 
+k = hello.length;
+for (var i = 0 ; i < k ; i++) {
+	console.log(hello[i].innerText);
 }
